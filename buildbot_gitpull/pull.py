c248c7ca83d6dd29577f9cfa482306c1425647f6
Quentin POIRIER <talanor@talanor.fr>
2020-06-12 10:39:58 +0200

    first commit

---
+++ buildbot_gitpull/pull.py
@@ -0,0 +1,75 @@
+from twisted.internet import defer
+from twisted.internet import reactor
+from twisted.python import log
+
+from buildbot import config as bbconfig
+from buildbot.interfaces import WorkerTooOldError
+from buildbot.process import buildstep
+from buildbot.steps.worker import CompositeStepMixin
+from buildbot.util.git import GitStepMixin
+
+class GitPull(buildstep.BuildStep, GitStepMixin, CompositeStepMixin):
+    description = None
+    descriptionDone = None
+    descriptionSuffix = None
+
+    name = 'gitpull'
+    renderables = ['repourl', 'branch', 'subtree']
+
+    def __init__(self, workdir=None, repourl=None, branch=None, subtree=None, force=False,
+                 env=None, timeout=20 * 60, logEnviron=True,
+                 sshPrivateKey=None, sshHostKey=None, sshKnownHosts=None,
+                 config=None, **kwargs):
+
+        self.workdir = workdir
+        self.repourl = repourl
+        self.branch = branch
+        self.force = force
+        self.env = env
+        self.timeout = timeout
+        self.logEnviron = logEnviron
+        self.sshPrivateKey = sshPrivateKey
+        self.sshHostKey = sshHostKey
+        self.sshKnownHosts = sshKnownHosts
+        self.config = config
+        self.subtree = subtree
+
+        super().__init__(**kwargs)
+
+        self.setupGitStep()
+
+        if not self.branch:
+            bbconfig.error('GitPull: must provide branch')
+
+    def _getSshDataWorkDir(self):
+        return self.workdir
+
+    @defer.inlineCallbacks
+    def run(self):
+        self.stdio_log = yield self.addLog("stdio")
+        try:
+            gitInstalled = yield self.checkFeatureSupport()
+
+            if not gitInstalled:
+                raise WorkerTooOldError("git is not installed on worker")
+
+            yield self._downloadSshPrivateKeyIfNeeded()
+            ret = yield self._doPull()
+            yield self._removeSshPrivateKeyIfNeeded()
+            return ret
+
+        except Exception as e:
+            yield self._removeSshPrivateKeyIfNeeded()
+            raise e
+
+    @defer.inlineCallbacks
+    def _doPull(self):
+        cmd = []
+        if self.subtree is not None:
+            cmd += ["subtree", 'pull', '--prefix', self.subtree]
+        else:
+            cmd += ['pull']
+        cmd += [self.repourl, self.branch]
+
+        ret = yield self._dovccmd(cmd)
+        return ret
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_description = "placeholder"
+
+VERSION = "1.0.0"
+
+setup(name='buildbot-gitpull',
+      version=VERSION,
+      description='buildbot plugin for pulling repos (and subtrees).',
+      author='Quentin POIRIER',
+      author_email='quentin.poirier@opus-solutions.eu',
+      url='https://github.com/placeholder',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['buildbot_gitpull'],
+      requires=[
+          "buildbot (>=2.0.0)"
+      ],
+      entry_points={
+          "buildbot.steps": [
+              "Gitea = buildbot_gitpull.pull:GitPull"
+          ],
+      },
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Plugins",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: MacOS",
+          "Operating System :: POSIX :: Linux",
+          "Topic :: Software Development :: Build Tools",
+      ]
+      )
